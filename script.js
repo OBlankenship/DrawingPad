23bf57a21131bb800c548ee63a4217857ce3b8d0
BLANKENSHIP <OLIVER.BLANKENSHIP@tccd.edu>
2021-12-15 13:12:32 -0600

    Added ability to change grid size and grid color

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 function generateSquares(numSquares) {
     let container = document.getElementById("container");
-    let perRow = Math.sqrt(numSquares);
-    let squareWidth = parseInt(99 / perRow)  + "%";
-    
+    removeAllChildNodes(container);
+    let squareWidth = parseInt(100 / numSquares)  + "%";
+    let colorField = document.getElementById("colorField");
+    let newColor = colorField.value;
     for (let i = 0; i < numSquares; i++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        square.style.width = squareWidth
-        square.style.opacity = "0%"
-        square.addEventListener('mouseover', hoverSquare);
-        container.appendChild(square);
+        for (let j = 0; j < numSquares; j++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = squareWidth
+            square.style.opacity = "0%"
+            square.style.background = newColor
+            square.addEventListener('mouseover', hoverSquare);
+            container.appendChild(square);
+        }
     }
 }
 
-generateSquares(600);
-
-function hoverSquare() {
-    let curOpacity = event.target.style.opacity;
-    curOpacity = Number(curOpacity)
-    curOpacity += .10;
-    event.target.style.opacity = curOpacity
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
+function hoverSquare(e) {
+    let curOpacity = e.target.style.opacity;
+    curOpacity = Number(curOpacity);
+    curOpacity += .10;
+    e.target.style.opacity = curOpacity;
+}
 
+function newGrid(e) {
+    let sizeField = document.getElementById("sizeField");
+    let newSize = sizeField.value;
+    generateSquares(newSize);
+}
+
+let btn = document.getElementById("sizeButton");
+btn.addEventListener('click', newGrid);
+
+generateSquares(25);
 
